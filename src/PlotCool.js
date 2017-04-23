a8036096f18cbf8fc20f529fa243ed284859a5ce
Carl Peaslee <cpeaslee@gmail.com>
2017-04-23 09:31:53 -0500

    first

---
+++ src/PlotCool.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PlotCool extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to plot.cool</h2>
+      </div>
+    )
+  }
+}
+
+export default PlotCool
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PlotCool from './PlotCool'
+
+ReactDOM.render(
+  <PlotCool />,
+  document.getElementById('root')
+)
